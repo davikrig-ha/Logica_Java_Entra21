2fec73d458e2588308e83ae4d6bd79cf5c147b82
Entra21 <entra21@proway.treina>
2021-05-06 21:22:25 -0300

    calculo de imc

---
+++ src/CalculoImc.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CalculoImc {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADA DE DADOS
+
+        System.out.println("Digite a sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite o seu peso");
+        double peso = scanner.nextDouble();
+
+
+        // PROCESSAMENTO
+
+        double resultado = (altura * altura) /peso;
+
+        // SAIDA DE DADOS
+
+        System.out.println("O resultado e " + resultado);
+
+
+
+
+
+
+
+    }
+}
