d45c1f3272500a633e64dca4671540a031ae171c
davikrig-ha <daviluia2@gmail.com>
2021-05-11 21:19:28 -0300

    nota sobre o trapesio

--- src/CalculaAreaDoTrapesio.java
+++ src/CalculaAreaDoTrapesio.java
@@ -14,3 +14,13 @@ public class CalculaAreaDoTrapesio {  public static void main(String[] args) {
 
 }
 }
+/*
+float b, B, h,area;
+b  = 6f;
+B = 8f
+h = 5f;
+area = (b + B) / 2f * h;
+System.out.printl(area);
+
+
+*/
