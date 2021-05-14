bae7f040b491fe8717fda3fc36be04aee36fe83a
davikrig-ha <daviluia2@gmail.com>
2021-05-14 14:23:31 -0300

    kilometro por hora

---
+++ src/Ex1014.java
@@ -0,0 +1,57 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/*Calcule o consumo médio de um automóvel sendo fornecidos a distância
+total percorrida (em Km) e o total de combustível gasto (em litros).
+
+Entrada
+O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km),
+ e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+Saída
+Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem
+ "km/l".
+
+Exemplo de Entrada	                                    Exemplo de Saída
+500
+35.0
+
+                                                          14.286 km/l
+
+2254
+124.4
+
+                                                          18.119 km/l
+
+4554
+464.6
+
+                                                           9.802 km/l
+
+
+ */
+
+
+public class Ex1014 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        //ENTRADA DE DADOS
+
+        int X = scanner.nextInt();
+        double Y = scanner.nextDouble();
+
+        //PROCESSAMENTO
+
+        double media = X / Y;
+
+        //SAIDA DE DADOS
+
+        System.out.printf("%.3f km/l", media);
+
+        scanner.close();
+
+    }
+}
