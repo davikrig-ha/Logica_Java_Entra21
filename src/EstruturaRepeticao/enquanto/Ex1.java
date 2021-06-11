7c246938479b3bd648385200ca4dd98d1f95b3c3
Your Name <you@example.com>
2021-06-11 20:18:29 -0300

    a

--- src/EstruturaRepeticao/enquanto/Ex1.java
+++ src/EstruturaRepeticao/enquanto/Ex1.java
@@ -1,6 +1,28 @@
 package EstruturaRepeticao.enquanto;
 
 import java.util.Scanner;
+/*
+URI 1113
+Leia uma quantidade indeterminada de duplas de valores inteiros X e Y.
+Escreva para cada X e Y uma mensagem que indique
+se estes valores foram digitados em ordem crescente ou decrescente.
+
+Entrada
+A entrada contém vários casos de teste. Cada caso contém dois valores inteiros X e Y.
+ A leitura deve ser encerrada ao ser fornecido valores iguais para X e Y.
+
+Saída
+Para cada caso de teste imprima “Crescente”, caso os valores tenham sido digitados na
+ordem crescente, caso contrário imprima a mensagem “Decrescente”.
+
+Entrada         Exemplo da saida
+5 4             Decrescente
+7 2             Decrescente
+3 8             Crescente
+2 2
+
+*/
+
 
 public class Ex1 {
     public static void main(String[] args) {
