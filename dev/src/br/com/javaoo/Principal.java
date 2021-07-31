93084f1c37b1f9abbb226bfaa594c03495b91491
Entra21 <entra21@proway.treina>
2021-07-30 21:20:58 -0300

    add

--- dev/src/br/com/javaoo/Principal.java
+++ dev/src/br/com/javaoo/Principal.java
@@ -2,6 +2,7 @@ package br.com.javaoo;
 
 import br.com.javaoo.comoo.Circulo;
 import br.com.javaoo.comoo.Triangulo;
+import br.com.javaoo.entities.Product;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -10,44 +11,21 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner scanner = new Scanner(System.in);
 
-        Circulo a = new Circulo();
 
-        System.out.println("Entre com o valor do raio do circulo um");
+        Product eletrodomestico = new Product();
 
-        a.raio = scanner.nextDouble();
+        eletrodomestico.setName("geladeira");
+        eletrodomestico.setPrice(2.000);
+        eletrodomestico.setQuantity(2);
 
-        Circulo b = new Circulo();
 
-        System.out.println("Entre com o valor do raio do circulo um");
-
-        b.raio = scanner.nextDouble();
-
-        a.calculaCirculo();
-        b.calculaCirculo();
+        System.out.println(eletrodomestico.getNAME());
+        System.out.println(eletrodomestico.getPrice());
+        System.out.println(eletrodomestico.getQuantity());
 
 
 
-        Triangulo x = new Triangulo();
 
-        System.out.println("Entre com os Valores de área do primeiro triangulo");
-
-        x.lado1 = scanner.nextDouble();
-        x.lado2 = scanner.nextDouble();
-        x.lado3 = scanner.nextDouble();
-
-        System.out.println("Entre com os Valores de área do segundo triangulo");
-        Triangulo y = new Triangulo();
-        y.lado1 = scanner.nextDouble();
-        y.lado2 = scanner.nextDouble();
-        y.lado3 = scanner.nextDouble();
-
-        x.calculaArea();
-        y.calculaArea();
-
-
-
-        scanner.close();
     }
 }
--- dev/src/br/com/javaoo/entities/Product.java
+++ dev/src/br/com/javaoo/entities/Product.java
@@ -1,4 +1,29 @@
 package br.com.javaoo.entities;
 
 public class Product {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+
+
+    public void setName(String nome){
+        this.name = nome;
+    }
+    public String getNAME(){
+       return name;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
 }
