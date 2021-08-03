d5af656b5293886ce7bda39b2bf9f7259b453c22
Entra21 <entra21@proway.treina>
2021-08-03 19:41:27 -0300

    orientaca a objetos

--- dev/src/br/com/javaoo/Principal.java
+++ dev/src/br/com/javaoo/Principal.java
@@ -1,31 +1,28 @@
 package br.com.javaoo;
 
-import br.com.javaoo.comoo.Circulo;
-import br.com.javaoo.comoo.Triangulo;
 import br.com.javaoo.entities.Product;
-
 import java.util.Locale;
-import java.util.Scanner;
-
 
 public class Principal {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
+        Product p = new Product();
 
-        Product eletrodomestico = new Product();
+        //setando os valores
 
-        eletrodomestico.setName("geladeira");
-        eletrodomestico.setPrice(2.000);
-        eletrodomestico.setQuantity(2);
+        p.setName("Cadeira");
+        p.setPrice(120.0);
+        p.setQuantity(2);
+        p.setColor("Vermelha");
 
+       //Pegando os valores
 
-        System.out.println(eletrodomestico.getNAME());
-        System.out.println(eletrodomestico.getPrice());
-        System.out.println(eletrodomestico.getQuantity());
-
-
-
+        System.out.println("Produto: " + p.getNAME());
+        System.out.println("Preço: " + p.getPrice() + " R$");
+        System.out.println("Quantidade: " + p.getQuantity());
+        System.out.println("Cor: " + p.getColor());
+        System.out.println("Quantidade em Estoque: " + p.totalValueInStock());
 
     }
 }
--- dev/src/br/com/javaoo/entities/Product.java
+++ dev/src/br/com/javaoo/entities/Product.java
@@ -2,28 +2,36 @@ package br.com.javaoo.entities;
 
 public class Product {
 
+    //declaração.
     private String name;
     private double price;
     private int quantity;
+    private String color;
 
 
-
+    //Setando e Capturando o objeto.
     public void setName(String nome){
         this.name = nome;
     }
     public String getNAME(){
        return name;
     }
+
     public void setPrice(double price){
         this.price = price;
     }
-    public double getPrice(){
-        return price;
-    }
+    public double getPrice(){ return price; }
+
     public void setQuantity(int quantity){
         this.quantity = quantity;
     }
     public int getQuantity(){
         return quantity;
     }
+
+
+    public void setColor(String color){ this.color = color; }
+    public String getColor(){ return color; }
+
+    public double totalValueInStock(){ return   quantity * price; }
 }
